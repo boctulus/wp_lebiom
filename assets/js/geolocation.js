f76ed67c3adfafe1e9e29fff069c890e8fb45a6e
Pablo Bozzolo <boctulus@gmail.com>
2023-04-29 09:51:34 +0800

    '.'

--- assets/js/geolocation.js
+++ assets/js/geolocation.js
@@ -61,3 +61,81 @@ async function get_geolocation() {
         Promise.reject(error);
     });
 }
+
+
+/*
+    Podria enviar al backend la localizacion
+*/
+
+//get_geolocation().then((ubicacion) => {
+    // console.log(ubicacion);
+//});
+
+
+/*
+    @param string country_code ej: 'CO'
+
+    Trabaja con CheckoutController@update_country
+
+    Podria ocupar callbacks asi:
+
+    update_country('CO', () => {
+        const countries_list = {
+            "es" : "España",
+            "pe" : "Perú",
+            "co" : "Colombia",
+            "mx" : "México",
+            "us" : "Estados Unidos"
+            // ....
+        }
+
+
+        jQuery('#billing_country').val(country_code) 
+        
+        // Campo especifico del tema de WP LEBIOM
+        jQuery('#billing_country_field').children('span').text(countries_list[country_code.toUpperCase()
+    })
+*/
+const update_country = function(country_code, cb_success = null, cb_fail = null){
+
+    country_code = country_code.toUpperCase()
+
+    jQuery.ajax({
+        url: '/api/checkout/country',
+        type: "patch",
+        dataType: 'json',
+        cache: false,
+        processData: false, // important
+        contentType: false, // important
+        data: JSON.stringify({country: country_code}),
+        success: function(res) {
+                // if is successful,...
+
+                if (cb_success != null){
+                    cb_success(country_code, res)
+                }
+
+                //clearAjaxNotification();
+                //setNotification(msg);			
+
+                console.log(res);                        
+            },
+            error: function(res) {
+                // if is a fail,...
+
+                if (cb_fail != null){
+                    cb_fail(country_code, res)
+                }
+
+                //clearAjaxNotification();
+
+                if (typeof res['message'] != 'undefined'){
+                    //setNotification(res['message']);
+                }
+
+                console.log(res);
+                console.log("An error occured, please try again.");         
+            }
+    });
+}   
+
--- main.php
+++ main.php
@@ -40,11 +40,93 @@ function assets(){
 enqueue('assets');
 
 
+add_action( 'woocommerce_checkout_before_order_review', 'my_custom_checkout_js' );
+
+function my_custom_checkout_js() {
+    ?>
+    <script>
+
+        let country;
+        
+        /*
+            Debende de como este configurado y plugins instalados
+        */
+        const payment_methods_by_country = {
+            'US' : [
+                'stripe'
+            ],
+
+            'MX' : [
+                'stripe_cc',
+                'stripe_oxxo',
+                'yith_pos_cash_gateway',
+                'bacs',
+                //'cod'
+            ]
+        }
+
+        /*
+            payment_method seria algo como
+
+            stripe                -US-
+            stripe_cc             -MX-
+            stripe_oxxo           -MX-
+            yith_pos_cash_gateway -MX-
+            bacs (transferencia)  -MX-
+
+            cod (contra re-embolso)
+        */
+        const hide_payment = (payment_method) => {
+            let __class = `.payment_method_${payment_method}`;
+            
+            //console.log('HIDDING .... ' + __class);
+            jQuery(__class).remove()
+        }
+
+
+        window.addEventListener("DOMContentLoaded", (event) => {            
+           
+            setTimeout(() => {  
+                //console.log('HIDDING ....');
+
+                country = jQuery('#billing_country').val();
+
+                switch(country){
+                    case 'MX':
+                        hide_payment('stripe')
+                        break;
+                    case 'US':
+                        hide_payment('stripe_cc')
+                        hide_payment('stripe_oxxo')
+                        hide_payment('yith_pos_cash_gateway')
+                        hide_payment('bacs')
+                        hide_payment('cheque')
+                        hide_payment('cod')
+                        break;
+
+                    default:
+                        hide_payment('stripe')
+                        hide_payment('stripe_cc')
+                        hide_payment('stripe_oxxo')
+                        hide_payment('yith_pos_cash_gateway')
+                        hide_payment('bacs')
+                        hide_payment('cheque')
+                        hide_payment('cod')
+                 }
+            }, 1000)
+        });
+
+    </script>
+    <?php
+}
+
 
 add_action('wp_loaded', function(){
     if (defined('WC_ABSPATH') && !is_admin())
 	{
-       // ...
+        $country = WC()->customer->get_billing_country();
+
+        dd($country, 'COUNTRY');
     }    
 });
 
